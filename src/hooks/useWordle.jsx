ba4e9e82f4b16d7a11855aa0f389569db897fad3
Tinodaishe Tembo <synthmancer@gmail.com>
2023-07-22 12:19:16 +0200

    wip

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -9,7 +9,9 @@ const useWordle = (solution) => {
 
 	// format a guess into an array of letter objects
 	// e.g [{key: 'a', color: 'yellow'}]
-	const formatGuess = () => {};
+	const formatGuess = () => {
+		console.log("Formatting the guess - ", currentGuess);
+	};
 
 	// add a new guess to the guesses state
 	// update the isCorrect state if the guess is correct
@@ -20,6 +22,28 @@ const useWordle = (solution) => {
 	// if user presses enter, add the new guess
 
 	const handleKeyUp = ({ key }) => {
+		if (key === "Enter") {
+			// only add guess if turn is less than 5
+			if (turn > 5) {
+				console.log("You used all your guesses");
+				return;
+			}
+
+			// do not allow duplicate words
+			if (history.includes(currentGuess)) {
+				console.log("You already tried that word");
+				return;
+			}
+
+			// check word is 5 chars
+			if (currentGuess.length !== 5) {
+				console.log("Word must be 5 chars long");
+				return;
+			}
+
+			formatGuess();
+		}
+
 		if (key === "Backspace") {
 			setCurrentGuess((prev) => {
 				return prev.slice(0, -1);
