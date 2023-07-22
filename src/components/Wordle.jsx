95edfd45ba67167e02e8d9a684e02355a452b409
Tinodaishe Tembo <synthmancer@gmail.com>
2023-07-22 13:50:13 +0200

    wip

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -2,13 +2,18 @@ import { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 
 const Wordle = ({ solution }) => {
-	const { currentGuess, handleKeyUp } = useWordle(solution);
+	const { currentGuess, handleKeyUp, guesses, isCorrect, turn } =
+		useWordle(solution);
 
 	useEffect(() => {
 		window.addEventListener("keyup", handleKeyUp);
 
 		return () => window.removeEventListener("keyup", handleKeyUp);
 	}, [handleKeyUp]);
+
+	useEffect(() => {
+		console.log(guesses, turn, isCorrect);
+	}, [guesses, turn, isCorrect]);
 
 	return (
 		<div>
