8cfbfb466cae18a9ba413272bc9b09e88f575bbc
Tinodaishe Tembo <synthmancer@gmail.com>
2023-07-23 18:00:47 +0200

    added a qwerty keypad

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -3,18 +3,56 @@ import React, { useEffect, useState } from "react";
 const Keypad = ({ usedKeys }) => {
 	const [letters, setLetters] = useState(null);
 
+	const [firstRowLetters, setFirstRowLetters] = useState(null);
+	const [secondRowLetters, setSecondRowLetters] = useState(null);
+	const [thirdRowLetters, setThirdRowLetters] = useState(null);
+
 	useEffect(() => {
-		fetch("http://localhost:3000/letters")
+		fetch("http://localhost:3000/first-row")
 			.then((res) => res.json())
 			.then((json) => {
-				setLetters(json);
+				setFirstRowLetters(json);
 			});
 	}, []);
 
+	useEffect(() => {
+		fetch("http://localhost:3000/second-row")
+			.then((res) => res.json())
+			.then((json) => {
+				setSecondRowLetters(json);
+			});
+	});
+
+	useEffect(() => {
+		fetch("http://localhost:3000/third-row")
+			.then((res) => res.json())
+			.then((json) => {
+				setThirdRowLetters(json);
+			});
+	});
+
 	return (
 		<div className="keypad">
-			{letters &&
-				letters.map((l) => {
+			{firstRowLetters &&
+				firstRowLetters.map((l) => {
+					const color = usedKeys[l.key];
+					return (
+						<div className={color} key={l.key}>
+							{l.key}
+						</div>
+					);
+				})}
+			{secondRowLetters &&
+				secondRowLetters.map((l) => {
+					const color = usedKeys[l.key];
+					return (
+						<div className={color} key={l.key}>
+							{l.key}
+						</div>
+					);
+				})}
+			{thirdRowLetters &&
+				thirdRowLetters.map((l) => {
 					const color = usedKeys[l.key];
 					return (
 						<div className={color} key={l.key}>
