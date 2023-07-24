61ffa081c0389f7f258e1b52eb4af5f5d0de6f3d
Tinodaishe Tembo <synthmancer@gmail.com>
2023-07-24 16:07:27 +0200

    wip

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/modal.css";
 
 const Modal = ({ isCorrect, turn, solution }) => {
+	const restart = () => {
+		// TODO FIX HERE
+		window.open(self);
+	};
+
 	return (
 		<div className="modal">
 			{isCorrect && (
 				<div>
-					<h1>You win!</h1>
+					<h2 class="win">You win!</h2>
 					<p className="solution">{solution}</p>
 					<p>You found the solution in {turn} guesses 😊</p>
+					<button className="btn" onClick={() => restart()}>
+						New Game
+					</button>
 				</div>
 			)}
 			{!isCorrect && (
 				<div>
-					<h1>Nevermind!</h1>
+					<h2 class="lose">Nevermind!</h2>
 					<p className="solution">{solution}</p>
 					<p>Better Luck next time 🤣</p>
+					<button className="btn" onClick={() => restart()}>
+						Try Again
+					</button>
 				</div>
 			)}
 		</div>
---
+++ todo.md
@@ -0,0 +1,8 @@
+## Todos
+
+- fix modal system.
+- ui fixes.
+- implement gradient color on the game title.
+- put actual icons for the menu and settings option
+- fix keyboard to be truly qwerty
+- implement a sound feedback system
